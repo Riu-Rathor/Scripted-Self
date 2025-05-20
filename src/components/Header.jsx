bd3875b4b2c0c30a16b9a64667ce7652c9db4046
Riya Saini <rsaini@opentext.com>
2025-05-20 10:17:34 +0530

    set up tailwind and create components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import LOGO from '../assets/logo.png';
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img src={LOGO}/>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Header
